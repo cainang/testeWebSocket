5913005fc7115d26d6ebe05bc84d27857aeef961
cainan43 <fabiocainan43@gmail.com>
2022-03-17 15:42:34 -0300

    add mod

--- index.js
+++ index.js
@@ -24,6 +24,8 @@ app.get('/', (req, res) => {
   res.send('<h1>Hello world</h1>');
 });
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+let port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+  console.log('listening on *: ' + port);
 });
