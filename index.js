8d6e8c85601e3030f1f39b7f4201a7994ca16e99
cainan43 <fabiocainan43@gmail.com>
2022-03-24 15:24:45 -0300

    add timeout para emitir evento

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
+const axios = require('axios');
 const app = express();
 const cors = require('cors');
-const http = require('http');
 app.use(cors('*'));
+const http = require('http').Server(app);
 
-const server = http.createServer(app);
+//const server = http.createServer(app);
 
-const { Server } = require("socket.io");
-const io = new Server(server, {
+const io = require("socket.io")(http, {
   cors: {
     origin: "*",
     methods: ["GET", "POST"]
@@ -15,17 +15,39 @@ const io = new Server(server, {
 
 io.on('connection', (socket) => {
     console.log('a user connected');
-    socket.on('chat message', (msg) => {
+    socket.on('event change', (msg) => {
       console.log('message: ' + msg);
     });
 });  
 
-app.get('/', (req, res) => {
+function getData(time = 0) {
+  setTimeout(async () => {
+    const response = await axios.get('https://radio.assembleiadedeusiadep.com/api/nowplaying/1');
+    io.emit('music change', JSON.stringify(response.data));
+
+    let played_at = response.data.playing_next.played_at;
+    let time_now = new Date().getTime();
+    let time_left = (played_at * 1000) - time_now;
+    console.log(time_left);
+    getData(time_left);
+  }, time);
+}
+
+getData();
+
+app.get('/', async (req, res) => {
+  const response = await axios.get('https://radio.assembleiadedeusiadep.com/api/nowplaying/1')
+  io.emit('music change', JSON.stringify(response.data));
   res.send('<h1>Hello world</h1>');
 });
 
-let port = process.env.PORT || 3000;
+app.post('/', (req, res) => {
+  io.emit('event change', req.body);
+  res.send('<h1>Hello world</h1>');
+});
 
-server.listen(port, () => {
+let port = 3000;
+
+http.listen(port, () => {
   console.log('listening on *: ' + port);
 });
